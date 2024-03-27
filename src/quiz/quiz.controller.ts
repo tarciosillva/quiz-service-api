a637dfca5adebab4483c6102ccbaa15eaab61be4
tarciosillva7 <tarciosillva7@gmail.com>
2024-03-26 23:19:13 -0300

    create quiz controller

---
+++ src/quiz/quiz.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { QuizService } from './quiz.service';
+import { CreateQuizDto } from './dto/create-quiz.dto';
+
+@Controller('quiz')
+export class QuizController {
+  constructor(private readonly quizService: QuizService) { }
+
+  @Post('/saveScore')
+  create(@Body() createQuizDto: CreateQuizDto) {
+    return this.quizService.create(createQuizDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.quizService.findAll();
+  }
+}
